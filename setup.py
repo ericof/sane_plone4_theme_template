a13b9e8a48e18ef22303eaea274fbd35c2c18a7f
Érico Andrei <erico@simplesconsultoria.com.br>
2012-03-11 22:40:04 -0700

    PEP8 and PyFlakes

--- setup.py
+++ setup.py
@@ -2,28 +2,28 @@
 
 from distutils.core import setup
 
-setup(name = "yourtheme",
-    version = "0.0",
-    description = "A Plone theme",
-    author = "",
-    author_email = "",
-    url = "",
-    install_require = ["five.grok"],
-    packages = ['yourtheme'],
-    classifiers=[
-        "Framework :: Plone",
-        "Framework :: Plone :: 4.1",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
-        "Operating System :: OS Independent",
-        "Programming Language :: JavaScript",        
-        "Programming Language :: Python",
-    ],     
-    license="GPL2",
-    include_package_data = True,
-    entry_points="""
-      # -*- Entry points: -*-
+setup(name="yourtheme",
+      version="0.0",
+      description="A Plone theme",
+      author="",
+      author_email="",
+      url="",
+      install_require=["five.grok"],
+      packages=['yourtheme'],
+      classifiers=[
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.1",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: JavaScript",
+          "Programming Language :: Python",
+      ],
+      license="GPL2",
+      include_package_data=True,
+      entry_points="""
+        # -*- Entry points: -*-
 
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,        
-) 
+        [z3c.autoinclude.plugin]
+        target = plone
+        """,
+)
