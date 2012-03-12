2d8c71819bbb097ed67e3bf328c8d48702a8030b
Érico Andrei <erico@simplesconsultoria.com.br>
2012-03-11 22:42:00 -0700

    PEP 8 and Pyflakes

--- yourtheme/interfaces.py
+++ yourtheme/interfaces.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 """
 
   For more information see
@@ -5,13 +6,14 @@
   * http://collective-docs.readthedocs.org/en/latest/components/interfaces.html
 
 """
-
 from plone.theme.interfaces import IDefaultPloneLayer
+
 
 class IThemeSpecific(IDefaultPloneLayer):
     """
-    Marker interface that defines a browser layer against which you can register views and viewlets.
+    Marker interface that defines a browser layer against which you can
+    register views and viewlets.
 
-    When your theme is selected this layer becomes active and all overrides associated with
-    override default Plone render actions.
+    When your theme is selected this layer becomes active and all overrides
+    associated with override default Plone render actions.
     """
